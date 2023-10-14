730cf1918e3cc4759ece1b8537101a8ef5b5869a
Momoyeyu <momoyeyu@outlook.com>
2023-10-15 00:51:16 +0800

    reset migration

--- backend/common/controller/team.py
+++ backend/common/controller/team.py
@@ -1,23 +1,25 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
-from common.controller.util import get_request_params
-import json
+from utils import get_request_params
+from common.models import Team
+from django.contrib.auth.models import User
 
 
 def dispatcher(request):
     # 将请求参数统一放入request 的 params 属性中，方便后续处理
 
+    # 经过此函数处理，request.params 里的对象已经转为 python 字典，数据类型也已经经过处理
     request.params = get_request_params(request)
 
     # 根据不同的action分派给不同的函数进行处理
     action = request.params['action']
-    if action == 'createteam':
+    if action == 'create_team':
         return create_team(request)
-    elif action == 'delteam':
+    elif action == 'del_team':
         return del_team(request)
-    elif action == 'jointeam':
+    elif action == 'join_team':
         return join_team(request)
-    elif action == 'quitteam':
+    elif action == 'quit_team':
         return quit_team(request)
 
     else:
@@ -25,11 +27,54 @@ def dispatcher(request):
 
 
 def create_team(request):
+    """
+    POST 获取数据样例：
+    {
+        "action":"create_team",
+        "data":{
+            "leader_id":"1",
+            "team_name":"ezctf",
+            "allow_join":"True"
+        }
+    }
+    注意 allow_join 的格式，我不知道 json 可不可以自动解析
+    """
+    info = request.params['data']
 
-    return HttpResponse()
+    # 检查用户是否在战队里
+    leader = User.objects.get(pk=info['leader_id'])
+    if leader.team_id is not None:
+        return JsonResponse({
+            'ret': 1,
+            'msg': '用户已有战队！'
+        })
+
+    #
+    record = Team.objects.create(
+        team_name=info['team_name'],
+        leader_id=info['leader_id'],
+        allow_join=info['allow_join'],
+        member_count=1
+    )
+
+    return JsonResponse({
+        'ret': 0,
+        'id': record.id
+    })
 
 
 def del_team(request):
+    """
+        DELETE 获取数据样例：
+        {
+            "action":"del_team",
+            "data":{
+                "leader_id":"1",
+                "team_name":"ezctf",
+            }
+        }
+        注意 allow_join 的格式，我不知道 json 可不可以自动解析
+        """
 
     return HttpResponse()
 
@@ -44,4 +89,21 @@ def join_team(request):
 def quit_team(request):
 
     return HttpResponse()
+
+
+def is_user_in_team(user_id, team_id):
+    """
+    if user in team
+        return true
+    else
+        return false
+    """
+    try:
+        user = User.objects.get(pk=user_id)
+        if user.team_id == team_id:  # 检查用户的team_id是否与给定的团队ID相匹配
+            return True
+        else:
+            return False
+    except User.DoesNotExist:
+        return False
 
--- backend/utils.py
+++ backend/utils.py
@@ -2,6 +2,12 @@ import json
 
 
 def get_request_params(request):
+    """
+    此函数处理不同 HTTP 请求 params 位置不同的问题，返回对应的 params
+    经过此函数处理，request.params 里的对象已经转为 python 字典，数据类型也已经经过处理
+    @param: request
+    @return: request.params
+    """
     if request.method == 'GET':
         params = request.GET
 
