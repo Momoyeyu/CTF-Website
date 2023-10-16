bffef0ef8e3b7d1e02ad87edbc49aa6ee76b9467
Momoyeyu <momoyeyu@outlook.com>
2023-10-16 22:35:02 +0800

    user新增api，rm __pycache__

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ezctf@outlook.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'
+EMAIL_HOST_PASSWORD = 'ezCtf11375'
 
 # Application definition
 
