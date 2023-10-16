9559d88c6aed830011b05664693e5cc7a610bbee
NinedreamGit <2281277289@qq.com>
2023-10-16 22:41:54 +0800

    完善admin.py

--- backend/rank/admin.py
+++ backend/rank/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-
-
+from rank.models import FirstKill
+admin.site.register(FirstKill)
--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from tasks.models import Task, AnswerRecord
+
+admin.site.register(Task)
+admin.site.register(AnswerRecord)
+from django.contrib import admin
+
+# Register your models here.
