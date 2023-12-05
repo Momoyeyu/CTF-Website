a925ef856f702621609bcd2991db2646579ef554
juanboy <2980526980@qq.com>
2023-12-05 19:50:39 +0800

    删除冗余项

--- ezctf.top/JS/indexpopup.js
+++
@@ -1,8 +0,0 @@
-function showpopup() {  
-  var popup = document.getElementById("popup-wrap");  
-  popup.style.display = "block";  
-}  
-function hidepopup() {  
-  var popup = document.getElementById("popup-wrap");  
-  popup.style.display = "none";  
-}
